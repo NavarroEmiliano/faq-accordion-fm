966f1a1bdaca92f65a9d6582b4e41242009d1c14
Emiliano Navarro <emilianonavarro1993@gmail.com>
2025-02-27 23:31:56 -0300

    toggle

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const d = document;
 const questionBtns = d.querySelectorAll(".faq__question");
+const allAnswers = d.querySelectorAll('.faq__answer')
 
 questionBtns.forEach((element) => {
   element.addEventListener("click", function () {
+    const minusIcon = element.querySelector('.icon-minus');
+    const plusIcon = element.querySelector('.icon-plus')
+    cleanAnswers()
+
+    minusIcon.classList.toggle('hidden');
+    plusIcon.classList.toggle('hidden');
     const answer = element.nextElementSibling
     answer.classList.toggle('show-answer')
   });
 });
+
+const cleanAnswers = () => {
+    allAnswers.forEach(element => {
+        element.classList.remove('show-answer')
+    })
+}
