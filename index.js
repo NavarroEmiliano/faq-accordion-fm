7b53f92298aa99eab2444fdfdec143001a33fd80
Emiliano Navarro <emilianonavarro1993@gmail.com>
2025-03-01 18:30:32 -0300

    logic

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const d = document;
 const faqList = d.querySelector(".faq__list");
 
-faqList.addEventListener("click", function (e) {
-  if(containsClass(e.target,'question') || containsClass(e.target,'faq__question') || containsClass(e.target,'icon-plus') || containsClass(e.target,'icon-minus')) {
-    console.log(true)
-  }
-});
+faqList.addEventListener("click", function(event) {
+  const btnClicked = event.target.closest(".faq__question");
 
-const containsClass = (target, className) => {
-  return target.classList.contains(className);
-};
+  if (!btnClicked) return;
+
+  const iconPlus = btnClicked.querySelector(".icon-plus");
+  const iconMinus = btnClicked.querySelector(".icon-minus");
+  const answer = btnClicked.nextElementSibling;
+
+  faqList.querySelectorAll(".faq__question").forEach((btn) => {
+    if (btn !== btnClicked) {
+      const otherIconPlus = btn.querySelector(".icon-plus");
+      const otherIconMinus = btn.querySelector(".icon-minus");
+      const otherAnswer = btn.nextElementSibling;
+
+      otherIconPlus.classList.remove("hidden");
+      otherIconMinus.classList.add("hidden");
+      otherAnswer.classList.remove("show-answer");
+    }
+  });
+
+  iconPlus.classList.toggle("hidden");
+  iconMinus.classList.toggle("hidden");
+  answer.classList.toggle("show-answer");
+});
